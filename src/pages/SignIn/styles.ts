043157db87ab043a191b21c317e2ac68f478e8c8
Diego Souza <dee-souza@hotmail.com>
2020-04-24 12:38:39 -0300

    :art: Page login in progress

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,99 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0px;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #f4ede8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+      border-radius: 10px;
+      border: 0px;
+      padding: 0px 16px;
+      width: 100%;
+      height: 56px;
+      color: #312e38;
+      margin-top: 16px;
+      font-weight: 500;
+      transition: all 0.25s ease-out;
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      margin-top: 24px;
+      display: block;
+      text-decoration: none;
+      transition: all 0.25s ease-out;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #ff9000;
+    margin-top: 24px;
+    display: block;
+    text-decoration: none;
+    transition: all 0.25s ease-out;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
